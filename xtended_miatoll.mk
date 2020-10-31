8c7911fdcdf20f526a92f0bf2887004b0fb887e9
Sonal Singh <sonal.singh.19993@gmail.com>
2020-10-31 18:41:05 +0530

    miatoll: Get Xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ xtended_miatoll.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 The Project-Xtended
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from miatoll  device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit some common xtended stuff
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Gapps
+ifeq ($(SHIP_GAPPS),true)
+$(call inherit-product-if-exists, vendor/google/gms/gms-vendor.mk)
+endif
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official
+XTENDED_BUILD_TYPE := OFFICIAL
+
+# Device identifier
+PRODUCT_NAME := xtended_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM6250
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer
+XTENDED_MAINTAINER := 🔥SonalSingh18🔥
