41ba4f44f7a163d0f24f61fac01f1147d5c42e44
GrettoRosella <rezarifaldy12@gmail.com>
2021-01-26 18:53:28 +0700

    miatoll: bringup revengeos

--- revengeos_miatoll.mk
+++ revengeos_miatoll.mk
@@ -4,10 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_miatoll
+# Inherit some common revengeos stuff
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official
+REVENGEOS_BUILDTYPE := OFFICIAL
+
+# Device identifier
+PRODUCT_NAME := revengeos_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
